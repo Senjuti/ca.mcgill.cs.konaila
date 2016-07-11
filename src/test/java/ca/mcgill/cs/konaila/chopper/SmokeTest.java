e6030108aaf1b970ed1e8a05671ef5d1c030f06a
Annie Ying <aying@us.ibm.com>
2016-07-11 12:38:34 -0400

    parser smoke test

--- src/test/java/ca/mcgill/cs/konaila/chopper/SmokeTest.java
+++ src/test/java/ca/mcgill/cs/konaila/chopper/SmokeTest.java
@@ -2,6 +2,7 @@ package ca.mcgill.cs.konaila.chopper;
 
 import java.util.Collection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SmokeTest {
@@ -9,8 +10,8 @@ public class SmokeTest {
 	@Test
 	public void test() {		
 		Collection<SelectionUnit> units = Chopper.parseAndChopFile(-1, "source1", 
-				"public class Test {\n public static void main(String[] args) {\n \n}", false, 0);
-		System.out.println();
+				"public class Test {\n public static void main(String[] args) {\n \n}\n}", false, 0);
+		Assert.assertEquals(2, units.size());
 	}
 
 }
